e7e574c8731246f0791b85770bf4c56f15ab5848
Margo Ray <margoxjoy@gmail.com>
2019-06-19 15:26:54 +0300

    Routing

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,14 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 019 19.06.19
- * Time: 14:54
- */
 
 namespace App\Controller;
-
-
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,5 +23,4 @@ class ArticleController
         return new Response(sprintf('All about Endgame in article: "%s"', $slug));
 
     }
-
 }
